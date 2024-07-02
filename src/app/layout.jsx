16aaf3184d196cfd7ad8f1f39ee1f702c9871008
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2024-07-02 08:26:49 -0300

    Adicionar cores e fonte do projeto

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,12 @@
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  style: ['italic', 'normal'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-poppins',
+});
 
 export const metadata = {
   title: 'Clinic Dashboard',
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        p_orange: '#D37045',
+        p_ligherblue: '#D3DFE8',
+        p_lightblue: '#45CAD3',
+        p_darkblue: '#38509F',
+        p_green: '#46D175',
+        p_red: '#F23E3E',
+        p_white: '#EBF4F4',
+        p_gray: '#535251',
+        p_graysecondary: '#645E5B',
+        p_black: '#171717',
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', 'system-ui', 'ui-monospace'],
+      },
     },
   },
   plugins: [],
