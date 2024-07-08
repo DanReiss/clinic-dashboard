592dee22e0c4cae4293298a11d14f2e24970bc7e
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2024-07-08 10:30:10 -0300

    Criar estrutura do formulário de login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,25 @@
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Form de Login
+    <main className="flex min-h-screen flex-row items-center justify-between">
+      <div className="flex-1" />
+      <div className="flex-1 grid items-center min-h-screen bg-p_white">
+        <form className="flex flex-col gap-7 max-w-[35rem] w-[75%] m-auto">
+          <div>
+            <h1 className="login-title text-3xl text-p_orange font-medium my-1">Bem vindo(a) de volta!</h1>
+            <h2 className="text-lg italic font-light my-1">Faça Log-in para continuar</h2>
+          </div>
+          <input type="text" placeholder="Email ou ID" className="block w-full border-2 border-p_graysecondary p-2 rounded" />
+          <input type="text" placeholder="Senha" className="block w-full border-2 border-p_graysecondary p-2 rounded" />
+          <div className="flex flex-row justify-between">
+            <label>
+              <input type="checkbox" className="mr-2" disabled />
+              Remember Me
+            </label>
+            <a href="/recovery" className="underline">Esqueceu a Senha?</a>
+          </div>
+          <button type="submit" className="w-[10rem] m-auto text-p_white bg-p_orange border-2 border-p_graysecondary p-2 rounded-full">Enviar</button>
+        </form>
+      </div>
     </main>
   );
 }
